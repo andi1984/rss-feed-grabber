d8e524f1fb2a626da8196171a33dfb06bd0ada0e
Andreas Sander <mail@andi1984.de>
2021-03-07 23:28:30 +0100

    Make vercel work

--- api/index.js
+++ api/index.js
@@ -1,78 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const cron = require("node-cron");
-const _ = require("lodash");
-
-const createClient = require("@supabase/supabase-js").createClient;
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_API_PUBLIC
-);
-
-const getFeed = require("../helper/getFeed");
-const stripFeedItem = require("../helper/stripFeedItem");
+const runCronJob = require("../helper/cron");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
-const tableName = process.env.SUPABASE_TABLE_NAME;
-
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Hello world");
+});
+app.get("/api/cron", async (req, res) => {
+  const data = await runCronJob();
+  return res.send(data);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-const cronJob = async () => {
-  console.log("running a task every minute");
-  // 1. Get current RSS feed
-  const rssData = await getFeed();
-
-  // 1b Strip out data we don't need
-  const strippedData = rssData.items.map(stripFeedItem).map((item) => ({
-    ..._.pick(item, ["title"]),
-    url: item.link,
-    id: item.guid,
-  }));
-
-  // 2. Filter out feed items hat are already in the DB
-
-  // Get overlapping IDs that are already in the DB
-  const allIds = strippedData.map((entry) => entry.id);
-  const {
-    data: overlappingIdsDbResult,
-    error: overlappingQueryError,
-  } = await supabase.from(tableName).select("id").in("id", allIds);
-
-  if (overlappingQueryError) {
-    throw new Error(overlappingQueryError.message);
-  }
-
-  const overlappingIds = overlappingIdsDbResult.map((entry) => entry.id);
-
-  const newData = strippedData.filter((item) => {
-    return !overlappingIds.includes(item.id);
-  });
-
-  if (newData.length === 0) {
-    return;
-  }
-
-  // 3. INSERT new items into the DB
-  const { data: insertedItems, error: insertionError } = await supabase
-    .from(tableName)
-    .insert(newData);
-
-  if (insertionError) {
-    throw new Error(insertionError.message);
-  }
-
-  console.log(`Inserted ${insertedItems.length} items to the DB.`);
-};
-
-console.log("Setup cronjob");
-// Run every 30 minutes
-cron.schedule("* * * * *", cronJob);
---
+++ helper/cron.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const _ = require("lodash");
+
+const createClient = require("@supabase/supabase-js").createClient;
+
+const getFeed = require("./getFeed");
+const stripFeedItem = require("./stripFeedItem");
+
+const runCronJob = async () => {
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_API_PUBLIC
+  );
+
+  const tableName = process.env.SUPABASE_TABLE_NAME;
+  // 1. Get current RSS feed
+  const rssData = await getFeed();
+
+  // 1b Strip out data we don't need
+  const strippedData = rssData.items.map(stripFeedItem).map((item) => ({
+    ..._.pick(item, ["title"]),
+    url: item.link,
+    id: item.guid,
+  }));
+
+  // 2. Filter out feed items hat are already in the DB
+
+  // Get overlapping IDs that are already in the DB
+  const allIds = strippedData.map((entry) => entry.id);
+  const {
+    data: overlappingIdsDbResult,
+    error: overlappingQueryError,
+  } = await supabase.from(tableName).select("id").in("id", allIds);
+
+  if (overlappingQueryError) {
+    throw new Error(overlappingQueryError.message);
+  }
+
+  const overlappingIds = overlappingIdsDbResult.map((entry) => entry.id);
+
+  const newData = strippedData.filter((item) => {
+    return !overlappingIds.includes(item.id);
+  });
+
+  if (newData.length === 0) {
+    console.log("return empty list");
+    return [];
+  }
+
+  console.log("AFTER EMPTY");
+
+  // 3. INSERT new items into the DB
+  const { data: insertedItems, error: insertionError } = await supabase
+    .from(tableName)
+    .insert(newData);
+
+  if (insertionError) {
+    throw new Error(insertionError.message);
+  }
+
+  console.log(`Inserted ${insertedItems.length} items to the DB.`);
+  return insertedItems;
+};
+
+module.exports = runCronJob;

24e47685c2983b722f72d5755f3363e21a847650
Andreas Sander <mail@andi1984.de>
2021-03-13 20:05:58 +0100

    Adding read API

--- api/index.js
+++ api/index.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const runCronJob = require("../helper/cron");
+const { getItems } = require("../helper/db");
 
 const app = express();
 const port = 8080;
 
-app.get("/api", (req, res) => {
-  res.send("Hello world!");
+const DEFAULT_LIMIT = 10;
+const DEFAULT_PAGE = undefined;
+
+app.get("/api", async (req, res) => {
+  // Access the provided 'page' and 'limt' query parameters
+  const page = parseInt(req.query.p);
+  const limit = parseInt(req.query.l);
+
+  const results = await getItems(
+    isNaN(limit) ? DEFAULT_LIMIT : limit,
+    isNaN(page) ? DEFAULT_PAGE : page
+  );
+  res.send(results);
 });
 app.get("/api/cron", async (req, res) => {
   const data = await runCronJob();
---
+++ helper/db.js
@@ -0,0 +1,29 @@
+const createClient = require("@supabase/supabase-js").createClient;
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_API_PUBLIC
+);
+const tableName = process.env.SUPABASE_TABLE_NAME;
+
+/**
+ * Get items from database.
+ * @param {number} limit
+ * @param {number} [offset]
+ * @returns {Object}
+ */
+async function getItems(limit, offset = 0) {
+  const request = await supabase
+    .from(tableName)
+    .select("*")
+    .range(offset, offset + (limit - 1));
+
+  if (request.error) {
+    throw new Error(request.error.message);
+  }
+
+  return request.data;
+}
+
+module.exports = {
+  getItems,
+};
